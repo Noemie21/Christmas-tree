2f7489848dacd39cb789ec61f285a8099de76f0f
Noémie <noemie.kpatenon@supinternet.fr>
2021-12-20 22:43:38 +0100

    app.py update

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ app = Flask(__name__)
 
 @app.route("/sapin/<id>")
 def christmas_tree(n):
-    max_width = 1 + 2 * 5 * 4
+    max_width = 1 + 2 * 4 * 4
     width = 1
-    for _ in range(n):
-        for _ in range(5):
+    for i in range(1, n+1):
+        for j in range(4):
             print(("*" * width).center(max_width))
             width += 2
         width -= 2 * 2
+    for x in range(i):
+        print(("|" *i).center(max_width))
+
